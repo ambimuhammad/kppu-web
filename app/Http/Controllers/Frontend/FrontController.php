f80d0e061862eabe6c9c369b0d1cad3845222681
Ambi Muhammad Dzuhri <ambi.muhammad26@gmail.com>
2019-12-14 00:38:06 +0700

    add about to front

--- app/Http/Controllers/Frontend/FrontController.php
+++ app/Http/Controllers/Frontend/FrontController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider\Slider;
+use App\Models\About\About;
 class FrontController extends Controller
 {
     public function index()
     {  
         $slides = Slider::all();
-        return view('front.front', compact('slides'));
+        $about = About::first();
+        return view('front.front', compact('slides', 'about'));
     }
 }
--- resources/views/front/front.blade.php
+++ resources/views/front/front.blade.php
@@ -121,14 +121,7 @@
 					<div>
 						<div class="space70"></div>
 						<h3>Who We Are &amp; What We Do</h3>
-						<p>Cum nascetur ridiculus mus. Prae sent non sem eu mi malesuada viverra volutpat ut libero.
-							Nullam a venenatis tellus. Nam fen convallis tristique. In imperdiet est sed aliquet
-							imperdiet. Nulla libero orci, cursus ut consecr ac, tempus a odio. Etiam quis massa ac ante
-							adipiscing consectetur. Duis dui turpis, porttitor sit amet metus sed, interdum tempus
-							ipsum. Integer aliquam sem elementum pellentesque. Donec mollis eros dolor, a ongue neque
-							venenatis vulputate. Etiam a eros adipiscing, dapibus ante id, luctus massa. Maecenas non
-							quam interdum, aliquam leo a, ornare mi. Integer. faucibus turpis sed leo gravida laoreet.
-							Curabitur at ligula. </p>
+						{!! htmlspecialchars_decode($about->deskripsi) !!}
 					</div>
 				</div>
 			</div>
