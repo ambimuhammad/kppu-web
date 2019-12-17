681e3ea5cadd9619e8c60e5426db71d232a00d6b
Ambi Muhammad Dzuhri <ambi.muhammad26@gmail.com>
2019-12-17 12:42:16 +0700

    hide wappalyzer

--- config/session.php
+++ config/session.php
@@ -124,10 +124,12 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    // 'cookie' => env(
+    //     'SESSION_COOKIE',
+    //     Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+    // ),
+
+    'cookie' => 'dirgatz_session',
 
     /*
     |--------------------------------------------------------------------------
